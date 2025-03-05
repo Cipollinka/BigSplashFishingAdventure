4281415bca634f93d6f93b3047f029acfb052145
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-03-05 23:17:41 +0200

    Update Params.js

--- src/Params.js
+++ src/Params.js
@@ -2,6 +2,6 @@ export default class Params {
   static keyPush = '17e204f0-2e4f-4683-a3aa-32f01a5dc108'; // ключ OneSignal
   static appID = 'id6742244200'; // ID додатку в сторі
   static keyApps = 'T9h7tLijQjDdZXpvgxh44L'; // ключ Appsflyer
-  static targetDate = '2025-02-28'; // дата закінчення відльожки
+  static targetDate = '2025-03-07'; // дата закінчення відльожки
   static bodyLin = 'https://splendid-exalted-celebration.space/N8Khb1hV'; // тіло лінки з якою будемо працювати
 }
